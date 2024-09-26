4e3f072397f32c75e348a910acb2c9948522c9dd
Mihyaeru <mihyaeru21@gmail.com>
2024-09-27 01:57:14 +0900

    Quit with ctrl-q

--- src/editor.rs
+++ src/editor.rs
@@ -1,30 +1,45 @@
 use crossterm::{
-    event::{read, Event, KeyCode},
+    event::{read, Event, KeyCode, KeyEvent, KeyModifiers},
     terminal::{disable_raw_mode, enable_raw_mode},
 };
 
 #[derive(Debug, Default)]
-pub struct Editor {}
+pub struct Editor {
+    shoud_quit: bool,
+}
 
 impl Editor {
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         if let Err(err) = self.repl() {
             panic!("{err:#?}");
         }
         print!("Goodbye.\r\n");
     }
 
-    fn repl(&self) -> Result<(), std::io::Error> {
+    fn repl(&mut self) -> Result<(), std::io::Error> {
         enable_raw_mode()?;
 
         loop {
-            if let Event::Key(event) = read()? {
-                println!("{event:?} \r");
+            if let Event::Key(KeyEvent {
+                code,
+                modifiers,
+                kind,
+                state,
+            }) = read()?
+            {
+                println!(
+                    "Code: {code:?} Modifiers: {modifiers:?} Kind: {kind:?} State: {state:?} \r",
+                );
 
-                if let KeyCode::Char(c) = event.code {
-                    if c == 'q' {
-                        break;
+                match code {
+                    KeyCode::Char('q') if modifiers == KeyModifiers::CONTROL => {
+                        self.shoud_quit = true;
                     }
+                    _ => {}
+                }
+
+                if self.shoud_quit {
+                    break;
                 }
             };
         }

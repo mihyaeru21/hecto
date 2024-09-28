5be4a6586ce83d030044ed99affb46b712a485f4
Mihyaeru <mihyaeru21@gmail.com>
2024-09-28 22:13:52 +0900

    Welcome message

--- src/editor.rs
+++ src/editor.rs
@@ -5,6 +5,9 @@ use std::io;
 use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyModifiers};
 use terminal::{Position, Terminal};
 
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 #[derive(Debug, Default)]
 pub struct Editor {
     shoud_quit: bool,
@@ -16,19 +19,6 @@ impl Editor {
         let result = self.repl();
         Terminal::terminate().unwrap();
         result.unwrap();
-    }
-
-    fn draw_rows() -> io::Result<()> {
-        let height = Terminal::size()?.height;
-        for row in 0..height {
-            Terminal::clear_line()?;
-            Terminal::print("~")?;
-            if row + 1 < height {
-                Terminal::print("\r\n")?;
-            }
-        }
-
-        Ok(())
     }
 
     fn repl(&mut self) -> io::Result<()> {
@@ -72,6 +62,46 @@ impl Editor {
 
         Terminal::show_cursor()?;
         Terminal::execute()?;
+        Ok(())
+    }
+
+    fn draw_rows() -> io::Result<()> {
+        let height = Terminal::size()?.height;
+        // we allow this since we don't care if our welcome message is put _exactly_ in the middle.
+        // it's allowed to be a bit up or down
+        #[allow(clippy::integer_division)]
+        let welcom_message_row = height / 3;
+        for row in 0..height {
+            Terminal::clear_line()?;
+            Self::draw_empty_row()?;
+            if row == welcom_message_row {
+                Self::draw_welcom_message()?;
+            }
+            if row.saturating_add(1) < height {
+                Terminal::print("\r\n")?;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn draw_empty_row() -> io::Result<()> {
+        Terminal::print("~")?;
+        Ok(())
+    }
+
+    fn draw_welcom_message() -> io::Result<()> {
+        let mut message = format!("{NAME} editor -- version {VERSION}");
+        let width = Terminal::size()?.width;
+        let len = message.len();
+        // we allow this since we don't care if our welcome message is put _exactly_ in the middle.
+        // it's allowed to be a bit to the left or right.
+        #[allow(clippy::integer_division)]
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        message = format!("{spaces}{message}");
+        message.truncate(width);
+        Terminal::print(message)?;
         Ok(())
     }
 }
--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,12 +1,22 @@
-use std::io::{self, stdout, Write};
+use std::{
+    fmt::Display,
+    io::{self, stdout, Write},
+};
 
 use crossterm::{
     cursor::{Hide, MoveTo, Show},
     queue,
     style::Print,
     terminal::{self, disable_raw_mode, enable_raw_mode, Clear, ClearType},
+    Command,
 };
 
+/// Represents the Terminal.
+/// Edge Case for platforms where `usize` < `u16`:
+/// Regardless of the actual size of the Terminal, this representation
+/// only spans over at most `usize::MAX` or `u16::size` rows/columns, whichever is smaller.
+/// Each size returned truncates to min(`usize::MAX`, `u16::MAX`)
+/// And should you attempt to set the cursor out of these bounds, it will also be truncated.
 pub struct Terminal {}
 
 impl Terminal {
@@ -23,18 +33,52 @@ impl Terminal {
     }
 
     pub fn clear_screen() -> io::Result<()> {
-        queue!(stdout(), Clear(ClearType::All))?;
+        Self::queue_command(Clear(ClearType::All))?;
         Ok(())
     }
 
     pub fn clear_line() -> io::Result<()> {
-        queue!(stdout(), Clear(ClearType::CurrentLine))?;
+        Self::queue_command(Clear(ClearType::CurrentLine))?;
         Ok(())
     }
 
-    pub fn print(message: &str) -> io::Result<()> {
-        queue!(stdout(), Print(message))?;
+    pub fn print<T: Display>(message: T) -> io::Result<()> {
+        Self::queue_command(Print(message))?;
         Ok(())
+    }
+
+    /// Moves the cursor to the given Position.
+    /// # Arguments
+    /// * `Position` - the  `Position`to move the cursor to. Will be truncated to `u16::MAX` if bigger.
+    pub fn move_cursor_to(position: Position) -> io::Result<()> {
+        // clippy::as_conversions: See doc above
+        #[allow(clippy::as_conversions, clippy::cast_possible_truncation)]
+        Self::queue_command(MoveTo(position.x as u16, position.y as u16))?;
+        Ok(())
+    }
+
+    pub fn hide_cursor() -> io::Result<()> {
+        Self::queue_command(Hide)?;
+        Ok(())
+    }
+
+    pub fn show_cursor() -> io::Result<()> {
+        Self::queue_command(Show)?;
+        Ok(())
+    }
+
+    /// Returns the current size of this Terminal.
+    /// Edge Case for systems with `usize` < `u16`:
+    /// * A `Size` representing the terminal size. Any coordinate `z` truncated to `usize` if `usize` < `z` < `u16`
+    pub fn size() -> io::Result<Size> {
+        let (width, height) = terminal::size()?;
+        Ok(Size {
+            // clippy::as_conversions: See doc above
+            #[allow(clippy::as_conversions)]
+            width: width as usize,
+            #[allow(clippy::as_conversions)]
+            height: height as usize,
+        })
     }
 
     pub fn execute() -> io::Result<()> {
@@ -42,37 +86,22 @@ impl Terminal {
         Ok(())
     }
 
-    pub fn move_cursor_to(position: Position) -> io::Result<()> {
-        queue!(stdout(), MoveTo(position.x, position.y))?;
+    fn queue_command<T: Command>(command: T) -> io::Result<()> {
+        queue!(stdout(), command)?;
         Ok(())
-    }
-
-    pub fn hide_cursor() -> io::Result<()> {
-        queue!(stdout(), Hide)?;
-        Ok(())
-    }
-
-    pub fn show_cursor() -> io::Result<()> {
-        queue!(stdout(), Show)?;
-        Ok(())
-    }
-
-    pub fn size() -> io::Result<Size> {
-        let (width, height) = terminal::size()?;
-        Ok(Size { width, height })
     }
 }
 
 #[derive(Debug, Clone, Copy)]
 pub struct Size {
-    pub width: u16,
-    pub height: u16,
+    pub width: usize,
+    pub height: usize,
 }
 
 #[derive(Debug, Clone, Copy)]
 pub struct Position {
-    pub x: u16,
-    pub y: u16,
+    pub x: usize,
+    pub y: usize,
 }
 
 impl Position {
